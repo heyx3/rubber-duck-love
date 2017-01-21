3cb40e3805d132b014561652a62c14143ffcc598
Chris Foster <fosterchris@comcast.net>
2017-01-20 23:11:42 -0500

    projectile starting

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ProjectileState
+{
+	
+}
+
 public class Projectile : MonoBehaviour
 {
+	[Header("State Variables")]
+	public float currTime;
+	public float totalThrowTime;
+	
+	[Header("Tuning Variables")]
+	public float speed = 10f;
+	public float maxThrowTime = 2f;
+	
 	public AnimationCurve arc = new AnimationCurve();
 
 	// Use this for initialization
