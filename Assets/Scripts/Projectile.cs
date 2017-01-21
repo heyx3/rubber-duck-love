29a2851dc28bddc8401717d3fb21b84c5221f602
Chris Foster <fosterchris@comcast.net>
2017-01-21 09:27:29 -0500

    Projectile physics that aren't garbage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,7 +6,8 @@ public enum ProjectileState
 {
 	Windup = 0,
 	Airborne = 1,
-	Landed = 2
+	Landed = 2,
+	Dead = 3
 }
 
 public class Projectile : MonoBehaviour
@@ -15,10 +16,17 @@ public class Projectile : MonoBehaviour
 	public ProjectileState currState = ProjectileState.Windup;
 	public float currTime;
 	public float totalThrowTime;
+	public float currThrowProgress;
+	public Vector3 startPos = new Vector3();
+	public Vector3 destPos = new Vector3();
+	public Vector3 startScale = new Vector3();
+	public Vector3 maxScale = new Vector3();
 	
 	[Header("Tuning Variables")]
 	public float speed = 10f;
+	public float minThrowTime = 0.5f;
 	public float maxThrowTime = 2f;
+	public float maxScaleMod = 1.5f;
 	
 	public AnimationCurve arc = new AnimationCurve();
 
@@ -31,10 +39,15 @@ public class Projectile : MonoBehaviour
 	
 	public void StartThrow(float power)
 	{
-		Debug.Log("Setting projectile power to " + power.ToString("f2"));
-		totalThrowTime = power * maxThrowTime;
+		//Debug.Log("Setting projectile power to " + power.ToString("f2"));
+		totalThrowTime = Mathf.Lerp(minThrowTime, maxThrowTime, power);
+		float realPower = totalThrowTime / maxThrowTime;
 		currTime = 0f;
 		currState = ProjectileState.Airborne;
+		startPos = transform.position;
+		destPos = transform.position + transform.up * totalThrowTime * speed;
+		startScale = transform.localScale;
+		maxScale = startScale * maxScaleMod * realPower;
 	}
 
 	// Update is called once per frame
@@ -50,9 +63,28 @@ public class Projectile : MonoBehaviour
 
 	void UpdateAirborne()
 	{
-		transform.Translate(transform.up * speed * Time.deltaTime);
 		currTime = currTime + Time.deltaTime;
+		currThrowProgress = currTime / totalThrowTime;
+		float scaleProgress = Mathf.SmoothStep(0,1,currThrowProgress);
+		float currScaleFactor = 1 - Mathf.Abs(scaleProgress - 0.5f) * 2;
+		float posProgress = (Mathf.SmoothStep(0, 1, 0.5f + currThrowProgress / 2) - 0.5f) * 2;
+		//Vector3 currPos = Vector3.Lerp(startPos,destPos,posProgress);
+		Vector3 currPos = Vector3.Lerp(startPos,destPos,scaleProgress);
+		Vector3 currScale = Vector3.Lerp(startScale,maxScale, currScaleFactor);
+		transform.position = currPos;
+		transform.localScale = currScale;
+		//transform.Translate(Vector3.up * speed * Time.deltaTime);
+
 		if (currTime >= totalThrowTime)
 			currState = ProjectileState.Landed;
+	}
+
+	void OnDrawGizmos()
+	{
+		if (currState == ProjectileState.Airborne)
+		{
+			Gizmos.color = Color.blue;
+			Gizmos.DrawWireSphere(destPos, 1f);
+		}
 	}
 }
