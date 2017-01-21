2673c013d848ce6d68339f24bc0df1f27c88a652
Chris Foster <fosterchris@comcast.net>
2017-01-21 14:19:15 -0500

    bobber fixes

--- Assets/Scripts/WaterRider.cs
+++ Assets/Scripts/WaterRider.cs
@@ -12,22 +12,26 @@ public class WaterRider : MonoBehaviour
 	private Vector2 currVel = new Vector2();
 
 	private WaterReader reader;
+	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start ()
 	{
-		reader = GetComponent<WaterReader>();	
+		reader = GetComponent<WaterReader>();
+		rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void FixedUpdate ()
 	{
 		// Vector2 waterDir = reader.tunedPushDir * maxSpeed;
 		// Vector2 waterVel = waterDir * Time.deltaTime;
 		// transform.position = transform.position + new Vector3(waterVel.x, waterVel.y, 0);
 		Vector2 waterAccel = reader.tunedPushDir * maxAccel;
-		currVel += waterAccel;
-		transform.position = transform.position + new Vector3(currVel.x, currVel.y, 0);
-		currVel *= 1 - dragFactor;
+		
+		rb2d.AddForce(waterAccel, ForceMode2D.Force);
+		//currVel += waterAccel;
+		//transform.position = transform.position + new Vector3(currVel.x, currVel.y, 0);
+		//currVel *= 1 - dragFactor;
 	}
 }
