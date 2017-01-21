1daf402e399b34f656711868b5508226440ced6b
Adam Cardoza <juliusblaise@gmail.com>
2017-01-21 16:14:21 -0500

    reduced file size of a few assets and made them not broken

---
+++ Assets/Scripts/AudioMgr.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SoundEffectType
+{
+    kThrow,
+    kSplash,
+    kQuack,
+    kSqueak,
+    kBoat,
+    kBoatHit,
+    kMine,
+    kMineExplode,
+    kVictoryStab,
+    kRockHit,
+    kDefeat
+}
+
+public class AudioMgr : MonoBehaviour
+{
+
+    public static AudioMgr Instance;
+
+    public AudioClip background;
+    public AudioClip[] music;
+    public AudioClip[] effects;
+
+    public AudioSource ambient;
+    public AudioSource bgm;
+    public AudioSource sfx;
+    public AudioSource thrower;
+
+    public ThrowControl player;
+    public bool throwing;
+
+    void Awake()
+    {
+        Instance = this;
+        Projectile.OnProjectileStageChange += Throw;
+    }
+
+    private void Throw(ProjectileState oldState, ProjectileState newState, ProjectileType type)
+    {
+        if (newState == ProjectileState.Airborne)
+        {
+            PlaySFX(SoundEffectType.kThrow);
+        }
+        if (newState == ProjectileState.Landed)
+        {
+            PlaySFX(SoundEffectType.kSplash);
+        }
+    }
+
+    void Start()
+    {
+        ambient.clip = background;
+        StartGame();
+    }
+
+    public void StopAll()
+    {
+        ambient.Stop();
+        bgm.Stop();
+    }
+
+    public void StartGame()
+    {
+        bgm.clip = music[0];
+        ambient.Play();
+        bgm.Play();
+    }
+
+    public void PlaySFX(SoundEffectType sfxType)
+    {
+        sfx.PlayOneShot(effects[(int)sfxType]);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Defeat();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Victory();
+        }
+        if (player.currState == PlayerState.Windup)
+        {
+            if (!throwing)
+            {
+                thrower.Play();
+                throwing = true;
+            }
+        }
+        if (player.currState != PlayerState.Windup)
+        {
+            if (throwing)
+            {
+                thrower.Stop();
+                throwing = false;
+            }
+        }
+
+    }
+
+    public void Victory()
+    {
+        PlaySFX(SoundEffectType.kVictoryStab);
+        bgm.clip = music[1];
+        ambient.Stop();
+        bgm.Play();
+    }
+
+    public void Defeat()
+    {
+        bgm.Stop();
+        PlaySFX(SoundEffectType.kDefeat);
+    }
+
+
+}
