25c4ce719fef82b3ab8844cc5d01bb31e89adbd5
Chris Foster <fosterchris@comcast.net>
2017-01-20 23:33:04 -0500

    Broken throw behavior

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public enum ProjectileState
 {
-	
+	Windup = 0,
+	Airborne = 1,
+	Landed = 2
 }
 
 public class Projectile : MonoBehaviour
 {
 	[Header("State Variables")]
+	public ProjectileState currState = ProjectileState.Windup;
 	public float currTime;
 	public float totalThrowTime;
 	
@@ -19,15 +22,37 @@ public class Projectile : MonoBehaviour
 	
 	public AnimationCurve arc = new AnimationCurve();
 
+
 	// Use this for initialization
 	void Start ()
 	{
 		
 	}
 	
+	public void StartThrow(float power)
+	{
+		Debug.Log("Setting projectile power to " + power.ToString("f2"));
+		totalThrowTime = power * maxThrowTime;
+		currTime = 0f;
+		currState = ProjectileState.Airborne;
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		switch(currState)
+		{
+		case ProjectileState.Airborne:
+			UpdateAirborne();
+			break;
+		}
+	}
+
+	void UpdateAirborne()
+	{
+		transform.Translate(transform.up * speed * Time.deltaTime);
+		currTime = currTime + Time.deltaTime;
+		if (currTime >= totalThrowTime)
+			currState = ProjectileState.Landed;
 	}
 }
