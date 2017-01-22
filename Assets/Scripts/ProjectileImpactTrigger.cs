16818ead01ad13a2fbe38d466db2d08cf2fb1e30
Chris Foster <fosterchris@comcast.net>
2017-01-21 20:51:06 -0500

    Projectile Impact Events and Impact Types

---
+++ Assets/Scripts/ProjectileImpactTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileImpactTrigger : MonoBehaviour {
+
+	private Projectile parentProj;
+	private bool readyForImpact = true;
+
+	void Start()
+	{
+		parentProj = transform.parent.GetComponent<Projectile>();
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		if (parentProj.currState != ProjectileState.Landed || !readyForImpact)
+			return;
+		
+		if (other.tag == "Player")
+		{
+			parentProj.ProcessImpact(ProjectileImpactType.RubberDuck);
+			readyForImpact = false;
+		}
+		else if (other.tag == "RealDuck" )
+		{
+			parentProj.ProcessImpact(ProjectileImpactType.RealDuck);
+			readyForImpact = false;
+		}
+	}
+}
