64d0497a06b48f44457518c1aabcdfe15e393f63
Chris Foster <fosterchris@comcast.net>
2017-01-22 00:01:13 -0500

    Win Panel in progress

--- Assets/Scripts/Game/UIManager.cs
+++ Assets/Scripts/Game/UIManager.cs
@@ -12,17 +12,29 @@ public class UIManager  : Singleton<UIManager>
 	public CanvasGroup continuePanel;
 	public Text rockCountLabel;
 
+	public Text levelPointsText;
+	public Text timeLabelText;
+	public Text timePointsText;
+	public Text rocksLabelText;
+	public Text rocksPointsText;
+	public Text totalAwardText;
+	public Text yourScoreText;
+
 
 	void OnEnable()
 	{
 		GameManager.OnGameStateChange += HandleGameStateChange;
 		GameManager.OnProjectileEvent += HandleProjectileEvent;
+		GameManager.OnWin += HandleGameManagerWin;
+		ScoreMgr.OnScoreManagerWinEvent += HandleScoreManagerWin;
 	}
 
 	void OnDisable()
 	{
 		GameManager.OnGameStateChange -= HandleGameStateChange;
 		GameManager.OnProjectileEvent -= HandleProjectileEvent;
+		GameManager.OnWin -= HandleGameManagerWin;
+		ScoreMgr.OnScoreManagerWinEvent -= HandleScoreManagerWin;
 	}
 
 	// Use this for initialization
@@ -60,4 +72,23 @@ public class UIManager  : Singleton<UIManager>
 	{
 		continuePanel.alpha = 1;
 	}
+
+	void HandleGameManagerWin(float time, int rocksLeft, int rockStarted)
+	{
+		timeLabelText.text = "TIME: " + time.ToString("f1") + " sec.";
+		rocksLabelText.text = "ROCKS LEFT: " + rocksLeft.ToString();
+
+	}
+
+	void HandleScoreManagerWin(int completionBonus, int rockBonus, int timeBonus,
+							   int scoreThisRound, int lastScore, int currentScore)
+	{
+		Debug.Log("Got ScoreManager Win!");
+		levelPointsText.text = "+ " + completionBonus.ToString("n0");
+		rocksPointsText.text = "+ " + rockBonus.ToString("n0");
+		timePointsText.text = "+ " + timeBonus.ToString("n0");
+		totalAwardText.text = "+ " + scoreThisRound.ToString("n0");
+		yourScoreText.text = currentScore.ToString("n0");
+	}
+
 }
