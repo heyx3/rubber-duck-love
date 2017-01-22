2e2adc44f3d90db9ec1f7029d7ac02d7551ee435
heyx3 <manning.w@husky.neu.edu>
2017-01-22 13:58:40 -0500

    Add shadows to things

---
+++ Assets/Scripts/Water/SpriteShadow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteShadow : MonoBehaviour
+{
+	public Sprite ShadowSprite;
+	public float ShadowDist = 0.1f;
+	public bool UseParentSpr = true;
+
+
+	public Transform MyTr { get; private set; }
+	public SpriteRenderer MySpr { get; private set; }
+
+	public SpriteRenderer ParentSpr { get; private set; }
+
+
+	private void Awake()
+	{
+		MyTr = transform;
+
+		ParentSpr = MyTr.parent.GetComponent<SpriteRenderer>();
+
+		MySpr = GetComponent<SpriteRenderer>();
+	}
+	private void LateUpdate()
+	{
+		Vector2 lightDir = ((Vector2)Water.Instance.LightDir).normalized;
+		MyTr.position = MyTr.parent.position - (Vector3)(lightDir * ShadowDist);
+
+		if (UseParentSpr)
+			ShadowSprite = ParentSpr.sprite;
+		MySpr.sprite = ShadowSprite;
+	}
+}
