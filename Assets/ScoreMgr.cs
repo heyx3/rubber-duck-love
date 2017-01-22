2e13d19eb284982b712c483177f04df1f0b7b1c8
Adam Cardoza <juliusblaise@gmail.com>
2017-01-21 21:10:35 -0500

    splash

---
+++ Assets/ScoreMgr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMgr : MonoBehaviour {
+
+    public int score;
+    public bool brokenShitIsFixed;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+
+}
