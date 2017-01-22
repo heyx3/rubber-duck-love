c9e106a191f03d8b847135adb036eed7c283c2b3
Chris Foster <fosterchris@comcast.net>
2017-01-22 11:04:29 -0500

    Added bounce trigger response for rocklobstacle

--- Assets/Scripts/Game/BounceTrigger.cs
+++ Assets/Scripts/Game/BounceTrigger.cs
@@ -16,8 +16,8 @@ public class BounceTrigger : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D col)
 	{
 		string col_tag = col.gameObject.tag;
-		Debug.Log("bounce off '" + col_tag + "'");
-		if (col_tag == "DuckBoundary" || col_tag == "Boat")
+		//Debug.Log("bounce off '" + col_tag + "'");
+		if (col_tag == "DuckBoundary" || col_tag == "Boat" || col_tag == "RockLobstacle")
 		{
 			if (OnBounce != null)
 				OnBounce(col_tag, gameObject.tag);
